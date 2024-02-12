080a35fc59c8a127636e80ce7354bd087482135d
Roberto Gonzalez <rgc00023@gmail.com>
2024-02-12 18:29:06 -0500

    este es un comentario

--- js/misnotas.js
+++ js/misnotas.js
@@ -10,8 +10,8 @@ function cargarNotas() {
   const listaNotas = obtenerNotas();
   const listaNotasElemento = document.getElementById("lista-notas");
   listaNotasElemento.innerHTML = "";
-  listaNotas.forEach((text) => {
-    const li = crearElementoNota(text);
+  listaNotas.forEach((text,index) => {
+    const li = crearElementoNota(text,index);
     listaNotasElemento.appendChild(li);
   });
 }
@@ -19,9 +19,10 @@ function cargarNotas() {
 function agregarNota() {
   const noteInput = document.getElementById("note-input");
   const notaText = noteInput.value;
-  if (notaText.trim() === "") {
-    return;
+  if (notaText.trim() === "") { 
+  return alert("Esta nota esta vacia");
   }
+  
 
   //Actualizar el local Storage
   const listaNotas = obtenerNotas();
@@ -54,22 +55,36 @@ function ajustarAlturaTextArea(textarea) {
   textarea.style.height = `${textarea.scrollHeight}px`;
 }
 
-function crearElementoNota(texto) {
+function crearElementoNota(texto,index) {
   const nuevaNota = document.createElement("li");
   const contentEl = document.createElement("pre");
-  //textarea.setAttribute("readonly", "true");
+ 
   nuevaNota.className = "note"
   contentEl.className = "note-content";
   contentEl.innerHTML = texto;
-  //ajustarAlturaTextArea(textarea); // Ajustar altura al cargar las notas
 
   const botonBorrar = document.createElement("button");
   botonBorrar.className = "boton-borrar";
   botonBorrar.textContent = "Borrar";
   botonBorrar.addEventListener("click", () => eliminarNota(nuevaNota));
 
+  const botonEditar = document.createElement("button");
+  botonEditar.className = "boton-borrar";
+  botonEditar.textContent = "Editar";
+  botonEditar.addEventListener("click", () => editaElemento(index));
+
+
   nuevaNota.appendChild(contentEl);
   nuevaNota.appendChild(botonBorrar);
+  nuevaNota.appendChild(botonEditar);
 
   return nuevaNota;
+}
+
+function editaElemento(index){
+  const listanueva = obtenerNotas();
+  listanueva[index] = prompt("Edita tu nota aqui");
+  localStorage.setItem("notas", JSON.stringify(listanueva));
+  cargarNotas();
+
 }
